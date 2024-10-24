d643ead89d919c2e15b46091609b79f71d4c45a6
Radoslav02 <radoslavpavkovg@gmail.com>
2024-10-24 10:46:54 +0200

    emailjs-com replaced with emailjs6

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,103 +1,101 @@
-// import { useState } from "react";
-// import { toast } from "react-toastify";
+import { useState } from "react";
+import emailjs from "@emailjs/browser"; // Import emailjs
+import { toast } from "react-toastify";
+import "./Contact.css";
 
- export default function ContactUs() {
-//   const [firstName, setFirstName] = useState("");
-//   const [lastName, setLastName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [message, setMessage] = useState("");
-//   const [isSending, setIsSending] = useState(false);
+export default function ContactUs() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSending, setIsSending] = useState(false);
 
-//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-//     if (!firstName || !lastName || !email || !message) {
-//       toast.error("All fields are required.");
-//       return;
-//     }
+    if (!firstName || !lastName || !email || !message) {
+      toast.error("All fields are required.");
+      return;
+    }
 
-//     setIsSending(true);
+    setIsSending(true);
 
-//     const templateParams = {
-//       from_name: `${firstName} ${lastName}`,
-//       from_email: email,
-//       message: message,
-//       to: 'your-email@example.com', // Replace with your email address
-//     };
+    const templateParams = {
+      from_name: `${firstName} ${lastName}`,
+      from_email: email,
+      message: message,
+    };
 
-//     try {
-//       const response = await emailjs.send(
-//         "YOUR_SERVICE_ID", // Replace with your EmailJS service ID
-//         "YOUR_TEMPLATE_ID", // Replace with your EmailJS template ID
-//         templateParams,
-//         "YOUR_USER_ID" // Replace with your EmailJS user ID
-//       );
+    emailjs
+      .send(
+        "service_zf9aerk",
+        "template_gdn2y8x",
+        templateParams,
+        "83kRfB6jgzmb21MF0"
+      )
+      .then((response) => {
+        console.log("Success:", response);
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setMessage("");
+        toast.success("Message sent successfully!");
+        setIsSending(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        toast.error("Failed to send message. Please try again.");
+        setIsSending(false);
+      });
+  };
 
-//       if (response.status === 200) {
-//         toast.success("Message sent successfully!");
-//         // Clear form fields
-//         setFirstName("");
-//         setLastName("");
-//         setEmail("");
-//         setMessage("");
-//       } else {
-//         throw new Error("Failed to send email");
-//       }
-//     } catch (error) {
-//       console.error("Error:", error);
-//       toast.error("Failed to send message. Please try again.");
-//     } finally {
-//       setIsSending(false);
-//     }
-//   };
-
-//   return (
-//     <div className="contact-page-container">
-//       <form className="contact-page-form" onSubmit={handleSubmit}>
-//         <h1>Kontaktirajte nas</h1>
-//         <div className="name-surname-contact-wrapper">
-//           <div className="contact-input-wrapper">
-//             <label>Ime:</label>
-//             <input
-//               type="text"
-//               value={firstName}
-//               onChange={(e) => setFirstName(e.target.value)}
-//               className="name-input"
-//             />
-//           </div>
-//           <div className="contact-input-wrapper">
-//             <label>Prezime:</label>
-//             <input
-//               type="text"
-//               value={lastName}
-//               onChange={(e) => setLastName(e.target.value)}
-//               className="surname-input"
-//             />
-//           </div>
-//         </div>
-//         <div className="contact-input-wrapper">
-//           <label>E-mail:</label>
-//           <input
-//             type="email"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//             className="contact-email-input"
-//           />
-//         </div>
-//         <div className="contact-input-wrapper">
-//           <label>Poruka:</label>
-//           <textarea
-//             value={message}
-//             onChange={(e) => setMessage(e.target.value)}
-//             className="message-area"
-//           ></textarea>
-//         </div>
-//         <div>
-//           <button className="send-message-button" type="submit" disabled={isSending}>
-//             {isSending ? "Slanje..." : "Pošaljite"}
-//           </button>
-//         </div>
-//       </form>
-//     </div>
-//   );
+  return (
+    <div className="contact-page-container">
+      <form className="contact-page-form" onSubmit={handleSubmit}>
+        <h1>Kontaktirajte nas</h1>
+        <div className="name-surname-contact-wrapper">
+          <div className="conatact-input-wrapper">
+            <label>Ime:</label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              className="name-input"
+            />
+          </div>
+          <div className="conatact-input-wrapper">
+            <label>Prezime:</label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className="surname-input"
+            />
+          </div>
+        </div>
+        <div className="conatact-input-wrapper">
+          <label>E-mail:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="contact-email-input"
+          />
+        </div>
+        <div className="conatact-input-wrapper">
+          <label>Poruka:</label>
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="message-area"
+          ></textarea>
+        </div>
+        <div>
+          <button className="send-message-button" type="submit" disabled={isSending}>
+            {isSending ? "Slanje..." : "Pošaljite"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -1,114 +1,110 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'; 
+import { RootState } from '../Redux/store';
+import { clearCart } from '../Redux/cartSlice'; 
+import "./Order.css"
 
-// import { useLocation, useNavigate } from 'react-router-dom';
-// import { useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { RootState } from '../Redux/store';
-// import { clearCart } from '../Redux/cartSlice';
-// import { SMTPClient } from 'emailjs'; // Import the SMTPClient class correctly
-// import "./Order.css";
+interface Customer {
+  email: string;
+  name: string;
+  number: string;
+  phoneNumber: string;
+  place: string;
+  postalCode: string;
+  street: string;
+  surname: string;
+}
 
-// interface Customer {
-//   email: string;
-//   name: string;
-//   number: string;
-//   phoneNumber: string;
-//   place: string;
-//   postalCode: string;
-//   street: string;
-//   surname: string;
-// }
+interface Item {
+  name: string;
+  size: string; 
+  price: number; 
+}
 
-// interface Item {
-//   name: string;
-//   size: string;
-//   price: number;
-// }
+const Order = () => {
+  const location = useLocation();
+  const navigate = useNavigate(); 
+  const dispatch = useDispatch(); 
 
- const Order = () => {
-//   const location = useLocation();
-//   const navigate = useNavigate();
-//   const dispatch = useDispatch();
+  const { customer, total } = location.state as {
+    customer: Customer; 
+    total: number; 
+  } || {}; 
 
-//   const { customer, total } = location.state as {
-//     customer: Customer;
-//     total: number;
-//   } || {};
+  const items = useSelector((state: RootState) => state.cart.items); 
 
-//   const items = useSelector((state: RootState) => state.cart.items);
+  const [isEmailSent, setIsEmailSent] = useState(false);
 
-//   const [isEmailSent, setIsEmailSent] = useState(false);
+  const sendEmail = () => {
+    if (!items || items.length === 0) {
+      alert("Nema poručenih proizvoda."); 
+      return;
+    }
 
-//   const sendEmail = () => {
-//     if (!items || items.length === 0) {
-//       alert("Nema poručenih proizvoda.");
-//       return;
-//     }
+    const templateParams = {
+      customerEmail: customer?.email,
+      customerName: customer?.name,
+      customerNumber: customer?.number,
+      customerPhoneNumber: customer?.phoneNumber,
+      customerPlace: customer?.place,
+      customerPostalCode: customer?.postalCode,
+      customerStreet: customer?.street,
+      customerSurname: customer?.surname,
+      total,
+      items: items.map((item: Item) => `Naziv: ${item.name}, Velicina: ${item.size}, Cena: ${item.price}`).join('\n'),
+    };
+  
+    emailjs.send('service_zf9aerk', 'template_gd7rbar', templateParams, '83kRfB6jgzmb21MF0')
+      .then(() => {
+        dispatch(clearCart());
+        setIsEmailSent(true);
+        navigate('/potvrda'); 
+      })
+      .catch((error) => {
+        console.error('Email sending error:', error);
+        alert(`Error: ${error.text || error.message}`);
+      });
+  };
 
-//     const client = new SMTPClient({
-//       user: 'web.shopg69@gmail.com', // Replace with your SMTP credentials
-//       password: 'gardinovci',
-//       host: 'smtp.gmail.com', // Replace with your email provider's SMTP host
-//       ssl: true, // Use SSL for secure connections
-//     });
+  const formatPrice = (price: number) => {
+    return new Intl.NumberFormat("sr-RS", {
+      style: "currency",
+      currency: "RSD",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(price);
+  };
 
-//     // Directly pass the values to client.send()
-//     client.send(
-//       {
-//         text: `Porudžbina za klijenta:\n\nIme: ${customer?.name}\nPrezime: ${customer?.surname}\nEmail: ${customer?.email}\nTelefon: ${customer?.phoneNumber}\nAdresa: ${customer?.street}, ${customer?.place} ${customer?.postalCode}\n\nProizvodi:\n${items.map((item: Item) => `Naziv: ${item.name}, Velicina: ${item.size}, Cena: ${item.price}`).join('\n')}\n\nUkupno za plaćanje: ${formatPrice(total)}`,
-//         from: 'web.shopg69@gmail.com', // Replace with the correct "from" address
-//         to: customer?.email, // Send the email to the customer
-//         subject: 'Potvrda porudžbine',
-//       },
-//       (err) => {
-//         if (err) {
-//           console.error('Email sending error:', err);
-//           alert(`Greška pri slanju emaila: ${err.message}`);
-//         } else {
-//           dispatch(clearCart());
-//           setIsEmailSent(true);
-//           navigate('/potvrda');
-//         }
-//       }
-//     );
-//   };
+  return (
+    <div className="order-container">
+      <h2>Detalji porudžbine</h2>
+      {customer ? (
+        <div className="customer-info-wrapper">
+          <h3>Podaci o klijentu:</h3>
+          <div className="show-profile">
+          <p>Email: {customer.email}</p>
+          <p>Ime: {customer.name}</p>
+          <p>Broj: {customer.number}</p>
+          <p>Telefon: {customer.phoneNumber}</p>
+          <p>Mesto: {customer.place}</p>
+          <p>Poštanski broj: {customer.postalCode}</p>
+          <p>Ulica: {customer.street}</p>
+          <p>Prezime: {customer.surname}</p>
+          <h3>Ukupno za plaćanje:{formatPrice(total)}</h3>
+          </div>
+        </div>
+      ) : (
+        <p>Nema podataka o klijentu.</p>
+      )}
+      <div className="order-button-wrapper">
+      <button className="back-button" onClick={() => navigate(-1)}>Nazad</button>
+       <button className="order-button" onClick={sendEmail}>Poruči</button>
+       {isEmailSent && <p>Poruka je uspešno poslata!</p>}
+       </div>
+    </div>
+  );
+};
 
-//   const formatPrice = (price: number) => {
-//     return new Intl.NumberFormat("sr-RS", {
-//       style: "currency",
-//       currency: "RSD",
-//       minimumFractionDigits: 2,
-//       maximumFractionDigits: 2,
-//     }).format(price);
-//   };
-
-//   return (
-//     <div className="order-container">
-//       <h2>Detalji porudžbine</h2>
-//       {customer ? (
-//         <div className="customer-info-wrapper">
-//           <h3>Podaci o klijentu:</h3>
-//           <div className="show-profile">
-//             <p>Email: {customer.email}</p>
-//             <p>Ime: {customer.name}</p>
-//             <p>Broj: {customer.number}</p>
-//             <p>Telefon: {customer.phoneNumber}</p>
-//             <p>Mesto: {customer.place}</p>
-//             <p>Poštanski broj: {customer.postalCode}</p>
-//             <p>Ulica: {customer.street}</p>
-//             <p>Prezime: {customer.surname}</p>
-//             <h3>Ukupno za plaćanje: {formatPrice(total)}</h3>
-//           </div>
-//         </div>
-//       ) : (
-//         <p>Nema podataka o klijentu.</p>
-//       )}
-//       <div className="order-button-wrapper">
-//         <button className="back-button" onClick={() => navigate(-1)}>Nazad</button>
-//         <button className="order-button" onClick={sendEmail}>Poruči</button>
-//         {isEmailSent && <p>Poruka je uspešno poslata!</p>}
-//       </div>
-//     </div>
-//   );
- };
-
- export default Order;
+export default Order;
